7af651fbd62e5fc470e3544dc5d835548ef32c9c
Aneta Nedelcheva <aneta.nedelcheva@gmail.com>
2016-06-12 19:57:13 +0300

    Adding solution to Point3D class, problem 1-4

---
+++ HW2-CSharpOOP/Point3D/Point3D/Path.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Point
+{
+    class Path
+    {
+        List<Point3D> points;
+
+        public Path()
+        {
+            this.points = new List<Point3D>();
+        }
+
+        public void addPoint(Point3D point)
+        {
+            points.Add(point);
+        }
+
+        public int numberOfPoints()
+        {
+            return points.Count;
+        }
+
+        public List<Point3D> Points
+        {
+            get
+            {
+                return points;
+            }
+        }
+
+        public override string ToString()
+        {
+ 	         return String.Join(Environment.NewLine, points);
+        }
+    }
+}
---
+++ HW2-CSharpOOP/Point3D/Point3D/PathStorage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Point
+{
+    static class PathStorage
+    {
+        public static void save(string path, Path storage)
+        {
+            StreamWriter writer;
+            using (writer = new StreamWriter(path))
+            {
+                writer.WriteLine(storage.ToString());
+            }
+        }
+        
+        private static Point3D parsePoint(string point)
+        {
+            double[] coordinates =
+                point
+                .Split(new Char[] { ' ', ',', '(', ')' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            return new Point3D(coordinates[0], coordinates[1], coordinates[2]);
+        }
+
+        public static Path load(string path)
+        {
+            Path storage = new Path();
+            StreamReader reader;
+            try
+            {
+                using (reader = new StreamReader(path))
+                {
+                    string[] points = 
+                        reader.ReadToEnd()
+                        .Split(Environment.NewLine.ToCharArray(), 
+                        StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var point in points)
+                    {
+                        storage.addPoint(parsePoint(point));
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File with path {0} cannot be found", path);
+            }
+            return storage;
+        }
+    }
+}
