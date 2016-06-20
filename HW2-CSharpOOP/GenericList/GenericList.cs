04c1536c80ed517f3ca4278eab6c885a78dcfd84
Aneta Nedelcheva <aneta.nedelcheva@gmail.com>
2016-06-20 14:07:17 +0300

    New methods added in GenericList

--- HW2-CSharpOOP/GenericList/GenericList.cs
+++ HW2-CSharpOOP/GenericList/GenericList.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace GenericList
 {
     class GenericList<T>
     {
-        private const int starting_index = 0;
-        private int index;
+        private int nextIndex = 0;
         private int capacity;
         private T[] array;
 
         public GenericList(int capacity)
         {
             this.capacity = capacity;
-            this.index = starting_index;
             array = new T[capacity];
         }
 
@@ -36,20 +31,82 @@ namespace GenericList
             }
         }
 
-        public int Index
+        public int NextIndex
         {
             get
             {
-                return index;
+                return nextIndex;
             }
             set
             {
-
+                this.nextIndex = value;
             }
         }
 
-        static void Main(string[] args)
+        public T this[int idx]
         {
+            get
+            {
+                if (idx < 0 || idx > this.nextIndex - 1)
+                {
+                    throw new IndexOutOfRangeException(String.Format("Index should be in the range [0, {0}]", this.nextIndex - 1));
+                }
+                return this.array[idx];
+            }
+
+            set
+            {
+                if (idx < 0 || idx > this.nextIndex - 1)
+                {
+                    throw new ArgumentOutOfRangeException(String.Format("Index should be in the range [0, {0}]", this.nextIndex - 1));
+                }
+
+                this.array[idx] = value;
+            }
+        }
+
+        public void Remove(int index)
+        {
+            if (index >= nextIndex)
+            {
+                throw new ArgumentOutOfRangeException(String.Format("An element ar position {0} doesn't exist", index));
+            }
+            for (int i = index; i < nextIndex && i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            this.nextIndex--;
+            this.array[nextIndex] = default(T);
+        }
+
+        public void Add(T element)
+        {
+            if (nextIndex == capacity)
+            {
+                T[] oldarray = this.array;
+                capacity *= 2;
+                this.array = new T[capacity];
+                Array.Copy(oldarray, this.array, this.nextIndex);
+            }
+            this.array[nextIndex] = element;
+            this.nextIndex++;
+        }
+
+        public void Insert(int index, T element)
+        {
+            if (this.nextIndex == this.array.Length)
+            {
+                T[] oldarray = this.array;
+                capacity *= 2;
+                this.array = new T[capacity];
+                Array.Copy(oldarray, this.array, this.nextIndex);
+            }
+            for (int i = this.nextIndex - 1; i >= index && i > 0; i--)
+            {
+                this.array[i] = this.array[i - 1];
+
+            }
+            this.nextIndex++;
         }
     }
 }
---
+++ HW2-CSharpOOP/GenericList/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericList
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            var list = new GenericList<int>(3);
+            list.Add(5);
+            list.Add(10);
+            list.Add(20);
+            list.Remove(1);
+            //Console.WriteLine(list[2]);
+        }
+    }
+}
